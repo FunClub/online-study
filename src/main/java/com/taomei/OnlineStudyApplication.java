754133098fd39cb63539f311f363830ce448e135
yehongmei <1348070618@qq.com>
2017-09-25 01:52:20 +0800

    update

--- src/main/java/com/taomei/OnlineStudyApplication.java
+++ src/main/java/com/taomei/OnlineStudyApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OnlineStudyApplication {
 
 	public static void main(String[] args) {
+		//1
         SpringApplication.run(OnlineStudyApplication.class, args);
 	}
 }
