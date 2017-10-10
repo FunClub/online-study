40113b98d602265c3143330cc74f7fa3f8766267
904620272@qq.com <aaa8824879>
2017-10-10 23:55:10 +0800

    update

---
+++ src/main/java/com/taomei/service/ImageService.java
@@ -0,0 +1,71 @@
+package com.taomei.service;
+
+import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.model.ObjectMetadata;
+import com.taomei.util.TimeUtil;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 图片服务
+ */
+@Service
+public class ImageService {
+    private static final String ENDPOINT="http://oss-cn-shenzhen.aliyuncs.com";
+    private static final String ACCESS_KEY_ID= "LTAIseZOgs0quMxr";
+    private static final String ACCESS_KEY_SECRET = "duoh4gnCVAIu8gvDJ1p7vE1pg0ARuw";
+    private static final String BUCKET_NAME ="taomei-oss";
+    private OSSClient client;
+    public ImageService() {
+        client =new OSSClient(ENDPOINT, ACCESS_KEY_ID, ACCESS_KEY_SECRET);
+    }
+
+
+    /**
+     * 删除图片
+     *
+     * @param imgPath 路片路径集合
+     * @return 统一对象
+     */
+   public void deleteImgs( List<String> imgPath) throws Exception {
+       for(String path:imgPath){
+           deleteImg(path);
+       }
+   }
+
+   public void deleteImg(String path) {
+       client.deleteObject(BUCKET_NAME, path);
+   }
+
+
+    /**
+     * 上传图片并获得路径
+     * @param files 上传的文件
+     * @return 图片地址集合
+     * @throws IOException 文件IO异常
+     */
+    public List<String> generateImgPaths(List<MultipartFile> files,String folder) throws IOException {
+        List<String> imgPath = new ArrayList<>();
+        ObjectMetadata meta = new ObjectMetadata();
+        for(MultipartFile file:files){
+            imgPath.add(generateImgPath(file,folder));
+        }
+        return imgPath;
+    }
+
+    public String generateImgPath (MultipartFile file,String folder) throws IOException {
+        ObjectMetadata meta = new ObjectMetadata();
+        InputStream inputStream = file.getInputStream();
+        String imageName =folder+"/"+ TimeUtil.getFileTime()+file.getOriginalFilename();
+        meta.setContentLength(file.getBytes().length);
+        client.putObject(BUCKET_NAME,imageName,inputStream,meta);
+        return imageName;
+    }
+
+
+}
--- src/main/java/com/taomei/web/share/contorller/ShareController.java
+++ src/main/java/com/taomei/web/share/contorller/ShareController.java
@@ -1,16 +1,18 @@
 package com.taomei.web.share.contorller;
 
+import com.taomei.service.ImageService;
 import com.taomei.web.share.data.ResultView;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.taomei.web.share.util.ValidateUtil;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/share")
@@ -35,5 +37,16 @@ public class ShareController {
     public Object validateVerificationCode(@PathVariable("verificationCode") String verificationCode, HttpSession session){
         String actualCode = session.getAttribute("verificationCode").toString();
         return actualCode.equals(verificationCode);
+    }
+    private final ImageService imageService;
+
+    @Autowired
+    public ShareController(ImageService imageService) {
+        this.imageService = imageService;
+    }
+
+    @PostMapping("files")
+    public Object uploadFiles(List<MultipartFile> files) throws IOException {
+        return this.imageService.generateImgPaths(files,"video");
     }
 }
